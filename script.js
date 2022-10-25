85ed9d72f948ffa7facd2fe15995746f95d6e35f
Christopher Curley <clevercurley@gmail.com>
2022-10-25 18:10:09 -0400

    adds renderPosts in script.js

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ async function main() {
   let users = await loadUsers();
   let posts = await loadPosts();
 
-  renderUsers(users);
-
-  // console.log("users: ", users);
-  // console.log("posts: ", posts);
+  renderUsers(users, posts);
 }
 
 async function loadUsers() {
@@ -31,20 +28,29 @@ async function loadPosts() {
   }
 }
 
-function renderUsers(users) {
+function renderUsers(users, posts) {
   const tbody = document.querySelector("tbody");
 
   users.map((user) => {
     const tr = document.createElement("tr");
     tr.innerHTML = `<td>${user.name}<td>`;
     tbody.append(tr);
-    tr.addEventListener("click", () => renderPosts(user.id));
+    tr.addEventListener("click", () => renderPosts(user.id, posts));
   });
 }
 
-function renderPosts(userId) {
-  //todo
-  console.log(userId);
+function renderPosts(userId, posts) {
+  const postsDiv = document.getElementById("posts");
+  postsDiv.innerHTML = ""; // remove any old content so that only one users posts are showing at a time.
+
+  usersPosts = posts.filter((post) => post.userId === userId);
+
+  usersPosts.map((post) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "card");
+    div.innerHTML = `<div class="card-body p-3 mb-2 bg-dark text-white"><h5 class="card-title">${post.title}</h5><p class="card-text">${post.body}</p></div>`;
+    postsDiv.append(div);
+  });
 }
 
 main();
