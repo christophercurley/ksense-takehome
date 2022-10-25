477eb83c17e437b379eee7029d35316b8456d979
Christopher Curley <clevercurley@gmail.com>
2022-10-25 15:36:53 -0400

    adds loadUsers and loadPosts functions to script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+
+async function main() {
+  let users = await loadUsers();
+  let posts = await loadPosts();
+
+  console.log("users: ", users);
+  console.log("posts: ", posts);
+}
+
+async function loadUsers() {
+  try {
+    const res = await fetch(USERS_URL);
+    data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function loadPosts() {
+  try {
+    const res = await fetch(POSTS_URL);
+    data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
